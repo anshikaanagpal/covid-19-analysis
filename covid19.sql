b15194e3707113d0495bca88788b088e0db6bb1c
anshikaanagpal <121786769+anshikaanagpal@users.noreply.github.com>
2023-07-04 22:14:13 +0300

    added.sql

--- covid19.sql
+++ covid19.sql
@@ -11,3 +11,11 @@ order by 1,2
 select location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
 from project..CovidDeaths
 order by 1,2
+
+-- Total Cases vs Population
+-- Shows what percentage of population infected with Covid
+
+Select Location, date, Population, total_cases,  (total_cases/population)*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+--Where location like '%states%'
+order by 1,2
